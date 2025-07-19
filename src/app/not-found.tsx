64e43af784dd476c962e0f2223c43c657255c975
Kiito Kurosawa <kiitokurosawa@gmail.com>
2025-07-19 20:42:30 +0900

    ビルドエラー解消

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '10vh' }}>
+      <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>404 - ページが見つかりません</h1>
+      <p>お探しのページは存在しないか、移動しました。</p>
+    </div>
+  );
+}
